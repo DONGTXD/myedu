ef04f0ba02f9b83bd55768498020c0e1177326bf
unknown <673430110@qq.com>
2019-03-21 15:40:52 +0800

    first commit

---
+++ tan-day01-python/base-type.py
@@ -0,0 +1,48 @@
+
+    #声明int_demo()方法
+def int_demo():
+    #声明aint变量并赋值为1
+    aint = 1
+    #打印anit变量
+    print(aint)
+    #打印aint变量类型；type(aint):获取aint类型
+    print(type(aint))
+
+#声明add
+def add(aint, bint):
+    # print(aint)
+    # print(bint)
+    return aint + bint
+
+
+def sub(aint, bint):
+    print(aint)
+    print(bint)
+    return aint - bint
+
+
+
+#声明一个float方法
+def float_demo():
+    #声明afloat变量，并赋值1.8
+    afloat = 1.8
+    #打印afloat变量
+    print(afloat)
+    #打印afloat变量的类型；type(afloat)：获取afloat类型
+    print(type(afloat))
+
+
+if __name__ == '__main__':
+
+    print(add(2, 3))
+     # float_demo()
+    # c = chengfa(5, 6)
+    # print(c)
+    # chengfa1 = chengfa(5, 6)
+    #
+    # print(type(c))
+    # a = add(1,654)
+    # print(a)
+    # print(add(1,654))
+
+    # int_demo()
